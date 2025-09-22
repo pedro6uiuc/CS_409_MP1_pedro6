287636bfc6f3b97ef0da61e03b3d9ff87d04dd7c
SAURON22 <pedro6@illinois.edu>
2025-09-22 04:30:36 +0200

    Second provisional version, added scrolling buttons and navbar resizing

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,28 @@
 /* Your JS here. */
-console.log('Hello World!')
+const btns = document.getElementById("navbar_text").children;
+const About = document.getElementById("About");
+const Computer_Science = document.getElementById("Computer_Science");
+const Hobbies = document.getElementById("Hobbies");
+const Navbar = document.getElementById("navbar");
+btns[0].addEventListener('click',()=>{
+    About.scrollIntoView();
+    Navbar.style.height = '10%';
+}); 
+btns[1].addEventListener('click',()=>{
+    Computer_Science.scrollIntoView();
+    Navbar.style.height = '10%';
+}); 
+btns[2].addEventListener('click', () =>{
+    Hobbies.scrollIntoView();
+    Navbar.style.height = '10%';
+})
+window.addEventListener('scroll', ()=>{
+        var max = window.innerHeight/5;
+        if (window.scrollY > max) {
+            navbar.style.height = '10%'; // Example: shrink padding
+            // Add other style changes here
+        } else {
+            navbar.style.height = '20%'; // Example: restore initial padding
+            // Restore other initial styles here
+        }
+})
